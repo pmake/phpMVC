6a4a6408128b115511558541a9a729f551221752
Archi <unique.ntsn+git@gmail.com>
2016-07-20 14:14:59 +0800

    定義不同網址(route)對應的需求處理方式

--- myLaravel/app/Http/routes.php
+++ myLaravel/app/Http/routes.php
@@ -11,6 +11,18 @@
 |
 */
 
+use App\Task;
+use Illuminate\Http\Request;
+
+//顯示請求處理
 Route::get('/', function () {
-    return view('welcome');
+    //return view('welcome');
+});
+//新增請求處理
+Route::post('/task', function(Request $request){
+    //
+});
+//刪除請求處理
+Route::delete('/task/{task}', function(Task $task){
+    //
 });
