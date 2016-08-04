4583742e223118a5f89f1487d87e04bea33d8b16
Archi <unique.ntsn+git@gmail.com>
2016-08-04 14:41:37 +0800

    修改shop view連結至index view的連結，改為路由

--- myLaravel/resources/views/shop.blade.php
+++ myLaravel/resources/views/shop.blade.php
@@ -2,7 +2,7 @@
 @section('title',"Shop Page")
 @section('price',"$100")
 @section("navbarList")
-    <li><a href="index.html">Home</a></li>
+    <li><a href="{{url("/")}}">Home</a></li>
     <li class="active"><a href="shop.html">Shop page</a></li>
     <li><a href="single-product.html">Single product</a></li>
     <li><a href="cart.html">Cart</a></li>
