a5865c2e5552d58ccc9d3586df68ef5886c884ad
Archi <unique.ntsn+git@gmail.com>
2016-08-10 16:52:26 +0800

    1、修改seeder檔案內容，加入想新增到資料表的內容 2、使用 "php artisan db:seed --class=drinkTableSeeder" 指令實作，實際產生影響。

--- myLaravel/database/seeds/drinkTableSeeder.php
+++ myLaravel/database/seeds/drinkTableSeeder.php
@@ -11,6 +11,13 @@ class drinkTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $fake_data = Faker\Factory::create();
+
+        DB::table("drinks")->insert([
+                "name" => $fake_data->name,
+                "comments" => $fake_data->realText($maxNbChars = 200, $indexSize = 2),
+                "rate" => rand(1,5),
+                "product_date" => date("Y-M-D"),
+            ]);
     }
 }
