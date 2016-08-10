09c4eceee28a6b12d89f11164925035ece229ed3
Archi <unique.ntsn+git@gmail.com>
2016-08-10 15:40:43 +0800

    1、在laravel根目錄使用 "composer require fzaninotto/faker --dev" 指令安裝假資料產生器套件 2、修改employees migration內容，在建立資料表同時將假資料產生器產生的資料寫入資料表

--- myLaravel/database/migrations/2016_08_10_070159_create_employees_table.php
+++ myLaravel/database/migrations/2016_08_10_070159_create_employees_table.php
@@ -19,7 +19,20 @@ class CreateEmployeesTable extends Migration
             $table->string('email')->unique();
             $table->timestamps();
         });
+
+        //實體化一個faker物件
+        $fake_data = Faker\Factory::create("zh_TW");
+        //使用DB物件的靜態方法table傳回的物件的insert方法將faker產生的假資料寫入資料庫
+        for($i=0; $i<10; $i++){
+            DB::table("employees")->insert([
+                "name" => $fake_data->name,
+                "phone_number" => $fake_data->phoneNumber,
+                "email" => $fake_data->emai
+            ]);
+        }
     }
+
+
 
     /**
      * Reverse the migrations.
