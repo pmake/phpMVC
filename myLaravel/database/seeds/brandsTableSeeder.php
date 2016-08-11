9ee2150d06b88222f35134972862b6750b4e2bd7
Archi <unique.ntsn+git@gmail.com>
2016-08-11 14:41:17 +0800

    再新增2個seeder，並寫入適當內容

---
+++ myLaravel/database/seeds/brandsTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class brandsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker\Factory::create("zh_TW");
+        $name = ["Esprits", "Lee", "Gucci"];
+
+        for($i=0; $i<10; $i++){
+            $productName = $name[$faker->numberBetween(0,2)];
+
+            DB::table("products")->insert([
+                'name' => $productName,
+                'created_at_ip' => $faker->ipv4,
+                'updated_at_ip' => $faker->ipv4,
+                'created_at' => $faker->date('Y-m-d', 'now'),
+                'updated_at' => $faker->date('Y-m-d', 'now')
+            ]);
+        }
+    }
+}
