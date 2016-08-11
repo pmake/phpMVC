17946357eb8f00437b9370c2900c5a749fe64b9f
Archi <unique.ntsn+git@gmail.com>
2016-08-11 14:28:59 +0800

    修改seeder檔案內容，加入更多欄位資料，然後執行產生資料列

--- myLaravel/database/seeds/productsTableSeeder.php
+++ myLaravel/database/seeds/productsTableSeeder.php
@@ -11,11 +11,26 @@ class productsTableSeeder extends Seeder
      */
     public function run()
     {
-        $fake_data = Faker\Factory::create('zh_TW');
+        $faker = Faker\Factory::create("zh_TW");
+        $name = ["jeans", "clothes", "hat"];
+        //$price = [399, 499, 599];
 
-        DB::table("products")->insert([
-            "name" => $fake_data->name,
-            "discribing" => $fake_data->realText($maxNbChars = 200, $indexSize = 2)
-        ]);
+        for($i=0; $i<10; $i++){
+            $no = $faker->ean8;
+            $productName = $name[$faker->numberBetween(0,2)];
+
+            DB::table("products")->insert([
+                'name' => "$productName-$no",
+                'title' => "$productName-$no", 
+                'discribing' => $faker->realText(200), 
+                'price' => $faker->numberBetween(399, 899),
+                'categoryID' => 1,
+                'brandID' => 1,
+                'created_at_ip' => $faker->ipv4,
+                'updated_at_ip' => $faker->ipv4,
+                'created_at' => $faker->date('Y-m-d', 'now'),
+                'updated_at' => $faker->date('Y-m-d', 'now'),
+            ]);
+        }
     }
 }
