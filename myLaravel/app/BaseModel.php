d4e7b73a5468295198e94a857886373c5650e6a2
Archi <unique.ntsn+git@gmail.com>
2016-08-11 16:46:25 +0800

    修正類別名稱

--- myLaravel/app/BaseModel.php
+++ myLaravel/app/BaseModel.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Product extends Model
+class BaseModel extends Model
 {
     public function selectQuery($queryString){
         return DB::select($queryString);
