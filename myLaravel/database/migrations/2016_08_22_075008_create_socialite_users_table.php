1cec813d6243f780f41f52e012299ea4cbbb02da
Archi <unique.ntsn+git@gmail.com>
2016-08-22 15:56:56 +0800

    1、使用artisan指令新增一個migration file用來建立社群登入用的資料表 2、修改檔案內容，加入需要的欄位 3、執行migrate指令產生資料表

---
+++ myLaravel/database/migrations/2016_08_22_075008_create_socialite_users_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSocialiteUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('socialite_users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('vender');
+            $table->integer('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('socialite_users');
+    }
+}
